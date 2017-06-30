7947d03d12a5c29f85756d8c2a2f185f30fe9652
Vishal Patel <vppatel@ualberta.ca>
2017-06-30 13:29:26 -0600

    Bug fixed and additional styling for TODO tab
    - Fixed input box duplication bug
    - Added additional styling for the todo tab and calendar tab

--- MyTodo.js
+++ MyTodo.js
@@ -1,43 +1,58 @@
 (function() {
 
+  //The heirarchy of the event binding functions is important, keep note of
+  //their nesting.
+  //Also css really hates numbers by themselves, so ensure they are appened
+  //to some string.
   window.onload = function(e) {
-    move();
+    var i = 0; //Currently used to differentiate "add" button, will be replaced with
+          // a label
+    //move();
 
     $( ".addCategory" ).click(function(){
 
       console.log("Running");
-      var panel = $('.category');
+      var panel = $('.todo');
 
+      var div = $('<div class="category cat' + i + '"></div>');
       var table = $('<table></table>');
       var row = $('<tr></tr>');
 
       var title = $('<p class="title">Homework</p>');
       var prgBar = $('<div id="myProgress"><div id="myBar"></div></div>');
-      var form = $('<form></form>');
+      var form = $('<form id="chkList"></form>');
 
       //Note: The text is wrapped in paragraph elements for easy styling.
       var chkBox = $('<td><input type="checkbox" name="homwork" value="Math"></td>');
       var chkText = $('<td><p class="chkText">Example</p></td>')
 
-      var button = $('<button class="add">Add</button>');
+      var button = $('<button class="add button' + i + '" + id="' + i + '">Add</button>');
+
 
       row.append(chkBox);
       row.append(chkText);
       table.append(row);
       form.append(table);
 
-      panel.append(title);
-      panel.append(prgBar);
-      panel.append(form);
-      panel.append(button);
+      div.append(title);
+      div.append(prgBar);
+      div.append(form);
+      div.append(button);
+
+      panel.prepend(div);
 
       //Bind add button functionality
-      $( ".add" ).click(function(){
+      $( ".add.button" + i ).click(function(){
 
         //Create and append a text area
-        var input = $("<textarea name='plans' rows='1'" +
-                     "cols='30' id='pls' class='scheduleText'></textarea><br>").text("Example");
-        input.insertBefore(this);
+        var input = $("<textarea name='plans' rows='2'" +
+                     "cols='30' class='scheduleText'></textarea><br>").text("Press enter when done.");
+
+        //Note: We cannot use numbers as an ID.
+        var btn = $(".button" + this.id);
+
+        //console.log(this.id);
+        input.insertBefore(btn);
 
         //Convert to a checkbox when the user presses enter.
         $( ".scheduleText" ).bind("keypress", function(e) {
@@ -49,9 +64,10 @@
             var chkBox = $('<td><input type="checkbox" ' +
                            'name="homwork" ' +
                            'value="Math"></td>');
-            var chkText = $('<td><p class="chkText">' + 'example' + '</p></td>')
-            var form = this.previousElementSibling;
-            var table = form.firstElementChild;
+            var chkText = $('<td><p class="chkText">' + 'example' + '</p></td>');
+
+            //Painstakingly finds the table
+            var table = $(this).siblings().filter("#chkList").find("table");
 
             row.append(chkBox);
             row.append(chkText);
@@ -71,7 +87,7 @@
         });
 
       });
-
+      i += 1; //Increment the counter
     });
 
 
